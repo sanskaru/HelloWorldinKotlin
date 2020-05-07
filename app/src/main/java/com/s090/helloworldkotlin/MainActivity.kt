610743aaa16ce766f353fdb7eaac2844bb2dbb93
sanskaru <hrsan420884@gmail.com>
2020-05-08 01:12:17 +0530

    First Commit

---
+++ app/src/main/java/com/s090/helloworldkotlin/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.s090.helloworldkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.setText("Hello World, in Kotlin tho")
+    }
+}
